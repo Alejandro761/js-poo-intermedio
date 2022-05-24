a8764a6225b6591726493b375fcbcfdb697a6724
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-05-23 21:35:48 -0500

    caracteristicas de propiedades de un objeto: configurable, enumerable and writable. clase 4

--- main.js
+++ main.js
@@ -16,5 +16,29 @@ const juan = {
 // console.log(Object.entries(juan)[3][0]);
 // console.log(Object.entries(juan)[3][1]);
 // console.log(Object.entries(juan)[3][1]('Curso 2'));
-console.log(Object.getOwnPropertyDescriptors(juan));   
 
+Object.defineProperty(juan, 'pruebaNASA', {
+    value: 'extraterrestres', enumerable: false, writable: false,
+    configurable: false
+});
+
+Object.defineProperty(juan, 'navigator', {
+    value: 'Firefox', enumerable: false, writable:true,
+    configurable: true
+});
+
+Object.defineProperty(juan, 'editor', {
+    value: 'vs-code', enumerable: true, writable:false,
+    configurable: true
+});
+
+Object.defineProperty(juan, 'terminal', {
+    value: 'bash', enumerable: true, writable:true,
+    configurable: false
+});
+
+console.log(Object.getOwnPropertyDescriptors(juan));
+
+Object.seal(juan) //pone todos sus atributos con configurable false, osea que no se pueden borrar
+
+Object.freeze(juan) //ademas de seal, también deja writable en false, por lo que los atributos del objeto no podran ser editados ni eliminados
