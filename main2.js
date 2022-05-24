ad63cc7ee3c451d99cbab93976e1787d35de886c
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-05-23 23:28:50 -0500

    deepCopy. clase9

--- main2.js
+++ main2.js
@@ -60,4 +60,49 @@ function recursiva(numerosArray) {
     } 
 }
 
-recursiva(numeros); 
+// recursiva(numeros); 
+
+function isObject(subject){
+    return typeof subject === 'object';
+}
+
+function isArray(subject){
+    return Array.isArray(subject);
+}
+
+function deepCopy(subject) {
+    let copySubject;
+
+    const subjectIsArray = isArray(subject);
+    const subjectIsObject = isObject(subject);
+
+    if(subjectIsArray) {
+        copySubject = [];
+    } else if(subjectIsObject){
+        copySubject = {};
+    } else {
+        return subject;
+    }
+
+    for (key in subject) {
+        const keyIsObject = isObject(subject[key]);
+        if(keyIsObject){
+            copySubject[key] = deepCopy(subject[key]);
+        } else {
+            if(subjectIsArray) {
+                copySubject.push(subject[key]);
+            } else {
+                copySubject[key] = subject[key];
+            }
+        }
+    }
+
+    return copySubject;
+}
+
+const objChido = deepCopy(obj1);
+console.log(objChido);
+objChido.a = 'asi';
+objChido.c.d = 'simon';
+console.log(objChido);
+console.log(obj1);
